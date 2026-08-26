d9747de8140871151db87209ceba4c4d56bbbf9a
Steven Stockhamer <stevestock@gmail.com>
2026-08-25 21:35:56 -0400

    Belles folly update
    Belles Folly changed to 1/75

--- src/main/java/com/peanubnutter/collectionlogluck/luck/LogItemInfo.java
+++ src/main/java/com/peanubnutter/collectionlogluck/luck/LogItemInfo.java
@@ -4307,7 +4307,7 @@ public class LogItemInfo {
     public static LogItemInfo JAR_OF_FEATHERS = new LogItemInfo("Jar of feathers",32921,
             new BinomialDrop(new RollInfo(LogItemSourceInfo.SHELLBANE_GRYPHON_KILLS, 1.0 / 2000)));
     public static LogItemInfo BELLES_FOLLY_TARNISHED = new LogItemInfo("Belle's folly (tarnished)",31245,
-            new BinomialDrop(new RollInfo(LogItemSourceInfo.SHELLBANE_GRYPHON_KILLS, 1.0 / 400)));
+            new BinomialDrop(new RollInfo(LogItemSourceInfo.SHELLBANE_GRYPHON_KILLS, 1.0 / 75)));
     public static LogItemInfo GRYPHON_FEATHER = new LogItemInfo("Gryphon feather",31235, new MissingKillCountDrop());
     public static LogItemInfo YAMI = new LogItemInfo("Yami",30888, new UnimplementedDrop());
     public static LogItemInfo CHASM_TELEPORT_SCROLL = new LogItemInfo("Chasm teleport scroll",30775, new UnimplementedDrop());
